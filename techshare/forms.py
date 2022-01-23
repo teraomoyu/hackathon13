27ffc94b33620cef50d2c8e7dd691819ca7389df
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-01-23 15:02:56 +0900

    commit

--- techshare/forms.py
+++ techshare/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from .models import Comment
 
 
 class SignUpForm(UserCreationForm):
@@ -13,7 +14,15 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ['username', 'password1', 'password2']
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["comment_content", "time_start", "time_end"]
 
 
-
+    
+class NewVideoForm(forms.Form):
+    
+    content = forms.CharField(label='Content', max_length=300)
+    file = forms.FileField()
 
--- techshare/urls.py
+++ techshare/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView, Home, DetailView
+from .views import SignUpView, Home, DetailView, BookmarkView, MyPostView,login,logout
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,6 +7,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('detail/<int:pk>', DetailView.as_view(), name='detail'),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/',login,name='login'),
+    path('logout/',logout,name='logout'),
+    path('bookmark/', BookmarkView.as_view(), name="bookmark"),
+    path('mypost/', MyPostView.as_view(), name="mypost"),
     path('', Home.as_view(), name='home')
 ]
 
